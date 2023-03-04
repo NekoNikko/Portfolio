1ac408c31de4d326aeca908400401c77ddaa9908
Marlon Argente <marlon.argente@gmail.com>
2023-03-04 21:32:50 +0800

    [DEV] add streamlit-lottie requirements.txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,14 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 
-with st.sidebar:
-	selected = option_menu(
-		menu_title="Main Menu",
+
+selected = option_menu(
+		menu_title=None,
 		options=["Home", "Project", "Contact"],
+		icons=["house", "book", "envelope"],
+	menu_icon="cast",
+	default_index=0,
+	orientation="horizontal",
 	)
 
 if selected == "Home":
